a1c2bfad64de7991eca5e406c0128eac0401068c
chenjiayao <chenjiayaooo@gmail.com>
2022-02-25 10:53:19 +0800

    add chapter 2 link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -35,6 +35,7 @@ module.exports = {
         sidebar: [
             { title: "首页", path: "/" },
             { title: "一切开始于网络编程", path: "/posts/chapter-1" },
+            { title: "Redis 通信协议", path: "/posts/chapter-2" },
         ]
     },
     locales: {
